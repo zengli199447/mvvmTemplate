b67145fffb8a7c8525fa5e775102fef8d16070ab
GitHub Action <action@github.com>
2021-10-14 09:21:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/zengli199447/mvvmtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zengli199447/mvvmtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zengli199447.mvvmtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zengli199447.mvvmtemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/zengli199447/mvvmtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zengli199447/mvvmtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zengli199447.mvvmtemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zengli199447.mvvmtemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
